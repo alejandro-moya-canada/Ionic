9e3b235ed331bf18b5a077c06411cf1b39259e0e
Alejandro Moya Cañada <alejandro.moya@nfoque.local>
2020-03-02 16:28:23 +0100

    formulario y lista de datos guardados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,51 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+/*
+const reasonInput = document.querySelector("#input-reason");
+const amountInput = document.querySelector("#input-amount");
+const cancelBtn = document.querySelector("#btn-cancel");
+const confirmBtn = document.querySelector("#btn-confirm");
+*/
 export class HomePage {
 
+  totalExpenses = 0;
+
   constructor() {}
+
+  addReason(event) {
+    // obtengo los valores introducidos en los input
+    let enteredReason = (<HTMLInputElement>document.getElementById("input-reason")).value;
+    let enteredAmount = (<HTMLInputElement>document.getElementById("input-amount")).value;
+
+    if( enteredReason.length <= 0 || enteredAmount.length <= 0 ) {
+      return;
+    }
+
+    console.log(enteredReason);
+    console.log(enteredAmount);
+
+    // creo la lista donde imprimir los datos
+    let newItem = document.createElement("ion-item");
+    newItem.textContent = enteredReason + ': $' + enteredAmount;
+    // capturo la lista y añado los valores
+    let expensesList = document.getElementById("expenses-list");
+    expensesList.appendChild(newItem);
+
+    // introduzco el precio final que se va acumulando
+    this.totalExpenses += +enteredAmount;
+    let totalExpensesOutput = (<HTMLInputElement>document.getElementById("total-expenses"));
+    let total = this.totalExpenses.toString();
+    totalExpensesOutput.innerText = total + " $";
+
+    this.clear();
+
+  }
+
+  clear() {
+    // borro los valores de los input
+    (<HTMLInputElement>document.getElementById("input-reason")).value = "";
+    (<HTMLInputElement>document.getElementById("input-amount")).value = "";
+  }
 
 }
